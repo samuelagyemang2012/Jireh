68387247183151c3c3fedb0dfa09de9ae016f40e
samuelagyemang2012 <khermz2012@gmail.com>
2017-06-15 14:57:06 +0100

    jireh

--- www/js/myscript.js
+++ www/js/myscript.js
@@ -227,6 +227,28 @@ function get_loans(email) {
         });
 }
 
+function displayImage(evt){
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+    // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            document.getElementById('img').src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+
+    }
+
+    // Not supported
+    else {
+        alert("oops");
+        // fallback -- perhaps submit the input to an iframe and temporarily store
+        // them on the server until the user's session ends.
+    }
+}
+
 
 
 
